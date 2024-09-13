d8167166cf95a1bf6f3a85287810fb4072cecc7a
承杰 徐 <sy599200076@gmail.com>
2024-09-13 16:35:38 +0800

    技能簡介更新

--- src/views/Achievement.vue
+++ src/views/Achievement.vue
@@ -1,19 +1,12 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
-    <h2>功能 3 </h2>
-    <p>{{ featureData }}</p>
+    <h2>技能簡介</h2>
+    <Achievement />
   </div>
-  <H1>
-          ...
-  </H1>
 </template>
 
 
 <script setup>
-import { ref } from 'vue';
-
-const featureData = ref('3始数据');
-
-// 可以在这里加载或处理该视图所需的数据
+import Achievement from '../components/Achievement.vue';
 </script>
