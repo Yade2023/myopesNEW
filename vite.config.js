926643b61d46395b14873e9d156e06de2d0c4c54
Yade2023 <144205116+Yade2023@users.noreply.github.com>
2025-10-14 02:11:15 +0800

    重新修改 BASH
    BASH
    VITECONFIG
    ROUTER

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Yade2023/',
   plugins: [
     AutoImport({
       resolvers: [ElementPlusResolver()],
