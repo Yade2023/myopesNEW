c34afe086e14a5039933cbaf37ebf0317520a0a6
Yade2023 <144205116+Yade2023@users.noreply.github.com>
2025-10-13 22:05:06 +0800

    33
    3

---
+++ src/components/Footers.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-column">
+        <p>Copyright@2025 徐承杰</p>
+      </div>
+
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  color: #000000;
+  padding: 10px 20px;
+  font-family: "Noto Sans TC", sans-serif;
+}
+
+.footer-container {
+  flex-wrap: wrap;
+}
+
+.footer-column {
+  display: flex;
+  justify-content: center;
+  flex: 1 1 150px;
+  margin: 10px 20px;
+}
+
+.footer-column h4 {
+  font-size: 16px;
+  margin-bottom: 12px;
+  color: #ffffff;
+  font-weight: bold;
+}
+</style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,21 +3,27 @@
 <template>
   <div class="layout">
     <header class="header">
-      <h1><RouterLink to="/" class="router-link-class">我的履歷頁</RouterLink></h1>
-      <button class="contact-button" @click="showContact = true">聯絡資訊</button>
-      <ContactPopup v-if="showContact" @close="showContact = false" />
-      
+      <h1>
+        <RouterLink to="/" class="router-link-class">我的履歷頁</RouterLink>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <RouterLink to="/Introduce" class="router-link-class">關於我</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/CurriculumVitae" class="router-link-class">個人履歷</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/Achievement" class="router-link-class">作品資料</RouterLink>
+          </li>
+        </ul>
+        <button class="contact-button" @click="showContact = true">聯絡資訊</button>
+        <ContactPopup v-if="showContact" @close="showContact = false" />
+      </nav>
+
     </header>
     <div class="main">
-      <aside class="sidebar">
-        <nav>
-          <ul>
-            <li><RouterLink to="/Introduce">自我介紹</RouterLink></li>&nbsp;
-            <li><RouterLink to="/CurriculumVitae">個人履歷</RouterLink></li>&nbsp;
-            <li><RouterLink to="/Achievement">作品資料</RouterLink></li>
-          </ul>
-        </nav>
-      </aside>
       <section class="content">
         <RouterView />
       </section>
